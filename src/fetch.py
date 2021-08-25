6aa8ffebf24ef5b6e85d876de1d41928b1c310f7
Rahul Singh Maharjan <rahul.maharjan.12114@ncit.edu.np>
2021-08-25 11:48:52 +0545

    fetch class

---
+++ src/fetch.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import helpers as hp
+import datetime as dt
+
+class Fetch():
+    def __init__(self):
+        super(Fetch, self).__init__()
+    
+    def extractSlides(self):
+        pass
+
+    def saveImages(self, save_image = True):
+        pass
+
+
+    def start(self):
+        pass
