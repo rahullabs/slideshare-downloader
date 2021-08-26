623d4a874d3fe69d5b5034bc2a55305a2ec03f80
Rahul Singh Maharjan <rahul.maharjan.12114@ncit.edu.np>
2021-08-26 10:11:47 +0545

    rmDir function created

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,10 @@
 import os
+import shutil
 
-def imageDirectory(folder):
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
+def imageDirectory(dir):
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+
+def rmdir(dir):
+    shutil.rmtree(dir)
+    
